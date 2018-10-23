fa859a2a44b4fa76ce2016d654fd33bd10a93e2b
Daniel Winkelman <dwinkelman3@gmail.com>
2018-10-23 15:54:20 -0400

    Additional documentation

--- tests/active_ir.c/active_ir.c.ino
+++ tests/active_ir.c/active_ir.c.ino
@@ -5,6 +5,9 @@ void setup() {
 
 void loop() {
     // Output voltage from IR sensor to analog pin
+    // High values are close, low values are far
+    // Device targets distances between 0.2 and 1.5 meters
+    // http://www.farnell.com/datasheets/1386113.pdf?_ga=2.110934277.1478548236.1540322089-1941483107.1539717387
     char buf[6];
     sprintf(buf, "%i", analogRead(0));
     Serial.println(buf);
