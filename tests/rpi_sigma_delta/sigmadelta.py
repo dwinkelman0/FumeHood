65092eab6f4707e60d756054bcf28d6bdca4b48c
Daniel Winkelman <dwinkelman3@gmail.com>
2018-10-28 15:01:56 +0000

    Tested timing of various calls

--- tests/rpi_sigma_delta/sigmadelta.py
+++ tests/rpi_sigma_delta/sigmadelta.py
@@ -1,8 +1,10 @@
 # picamera library source can be found at
 # https://github.com/waveform80/picamera
+# Documentation can be found at
+# picamera.readthedocs.io/en/release-1.13
 
 from picamera import PiCamera
-from time import sleep
+from time import sleep, time
 from io import BytesIO
 from PIL import Image
 from numpy import asarray
@@ -14,13 +16,23 @@ camera = PiCamera()
 sleep(2)
 
 # Capture frame to byte stream
-camera.capture(stream, format='jpeg')
+t0 = time()
+camera.capture(stream, format="bmp")
+t1 = time()
+print "Capture:", t1 - t0
 stream.seek(0)
+t2 = time()
 image = Image.open(stream)
+t3 = time()
+print "Image.open():", t3 - t2
 image.save("shot.png")
+t4 = time()
+print "Image.save():", t4 - t3
 
 # Convert frame to a Numpy array
 array = asarray(image)
+t5 = time()
+print "numpy.asarray():", t5 - t4
 
 # Clean up
 camera.close()
