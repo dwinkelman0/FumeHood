6f3dff930cc76eaacb8d8c389fd2b4f23459ca94
Daniel Winkelman <dwinkelman3@gmail.com>
2018-10-29 04:13:58 +0000

    Primitive camera motion detection  * Uses sum of absolute value of differences between individual pixels

--- tests/rpi_sigma_delta/sigmadelta.py
+++ tests/rpi_sigma_delta/sigmadelta.py
@@ -1,6 +1,7 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import numpy as np
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -9,7 +10,17 @@ raw_frame = PiRGBArray(camera, size=camera.resolution)
 
 time.sleep(0.1)
 
+last_image = None
+
 for frame in camera.capture_continuous(raw_frame, format="bgr", use_video_port=True):
 	image = frame.array
-	print image
+	image = image.astype(int)
+	if last_image is not None:
+		difference = image - last_image
+		sqr_difference = np.abs(difference)
+		subtotal = np.sum(np.sum(sqr_difference))
+		subtotal_long = subtotal.astype(np.int64)
+		total = np.sum(subtotal_long)
+		print "Weighted Difference:", np.log(total)
+	last_image = image
 	raw_frame.truncate(0)
