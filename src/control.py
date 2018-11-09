8ddb03f01f295bb6210b4b06843c1266ed988d60
Daniel Winkelman <dwinkelman3@gmail.com>
2018-11-09 21:40:55 +0000

    Sigma delta algorithm implementation

--- src/control.py
+++ src/control.py
@@ -15,30 +15,96 @@
 #   manual override mechanism.
 
 from camera import Camera
+import polygon
+
+import numpy as np
 import threading
 import time
+import math
 
-def MonitorFrames(camera, cond_callback):
+# Interrupt codes for the control thread
+INTERRUPT_NONE = 0
+INTERRUPT_ACTIVITY = 1
+INTERRUPT_MANUAL_OVERRIDE = 2
+INTERRUPT_SASH_CLOSED = 3
+
+# GPIO pins
+
+class ControlThread(threading.Thread):
+	def __init__(self):
+		super(ControlThread, self).__init__(target=self.Main)
+		self.cond_interrupt = threading.Condition()
+		self.interrupt_code = INTERRUPT_NONE
+
+	def Main(self):
+		while True:
+			with self.cond_interrupt:
+				# Wait until an interrupt is called
+				self.cond_interrupt.wait()
+				code = self.interrupt_code
+
+				print("Activity Interrupt")
+
+def MonitorFrames(camera, control):
 	'''Compare current frame to last frame as they are generated'''
+
+	# Storage for frames to compare
 	current_frame, last_frame = None, None
-	t0 = time.time()
-	n_frames = 0
+	width, height = camera.camera.resolution
+
+	# Generate mask for desired camera range
+	mask = None
+	try:
+		# Try to read polygon from file and build a mask array
+		with open("camserver/points.json") as polygon_file:
+			print("Starting to create mask...")
+			pts_str = polygon_file.read()
+			pts = [polygon.Point(i["x"], i["y"]) for i in eval(pts_str)]
+			shape = polygon.Polygon(pts)
+			
+			mask = shape.GenerateMask(width, height)
+			mask_weight = np.sum(np.sum(np.sum(mask)))
+			print("Created mask")
+	except:
+		print("Error with creating polygon mask")
+
 	while True:
 		with camera.stream.cond_newframe:
-			n_frames += 1
-			print(n_frames / (time.time() - t0))
+			# Wait for a new frame
 			camera.stream.cond_newframe.wait()
 			last_frame = current_frame
-			current_frame = camera.stream.frame
-		if not last_frame is None:
-			# Perform sigma delta check
-			pass
-			"""activity = False
-			if activity:
-				with cond_callback:
-					cond_callback.notify_all()"""
+			# Cast frame into a numpy array
+			current_frame = np.frombuffer(camera.stream.frame, dtype="uint8")
+			current_frame.shape = (height, width, 3)
+
+		# Perform sigma delta check
+		if mask is not None:
+			current_frame = np.multiply(current_frame, mask)
+		current_frame = current_frame.astype("int32")
+		if last_frame is not None:
+			difference = np.abs(current_frame - last_frame)
+			subtotal = np.sum(np.sum(difference)).astype("int64")
+			total = np.sum(subtotal) / mask_weight # Average change per pixel
+			print("Activity: %.3f" % total)
+			activity = total > 5 # Completely arbitrary
+		else:
+			activity = False
+
+		if activity:
+			# Send a signal to the control thread
+			with control.cond_interrupt:
+				control.interrupt_code = INTERRUPT_ACTIVITY
+				control.cond_interrupt.notify_all()
+
+def MonitorGPIO(control):
+	'''Monitor GPIO inputs for manual override and motor encoder'''
+	
 
 def Main():
+	# Create the main control thread
+	control_thread = ControlThread()
+	control_thread.start()
+
 	# Create a camera with an enclosed frame stream
 	camera = Camera()
 
@@ -46,7 +112,7 @@ def Main():
 	threading.Thread(target=camera.GetFrames).start()
 
 	# Start the frames processing thread
-	threading.Thread(target=MonitorFrames, args=(camera, None)).start()
+	threading.Thread(target=MonitorFrames, args=(camera, control_thread)).start()
 
 if __name__ == "__main__":
 	Main()
---
+++ src/polygon.py
@@ -0,0 +1,76 @@
+# Resources:
+# https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon
+
+import numpy as np
+
+class Point(object):
+	def __init__(self, x, y):
+		self.x, self.y = float(x), float(y)
+
+	def __repr__(self):
+		return "(%.3f, %.3f)" % (self.x, self.y)
+
+class Equation(object):
+	def __init__(self, p1, p2):
+		# ax + by = c
+		# Transform point-slope form into standard form (for use in matrices)
+		# y - y0 = (dy/dx)(x - x0) ---> (dx)y - (dy)x = (dx)y0 - (dy)x0
+		dx, dy = p2.x - p1.x, p2.y - p1.y
+		self.A, self.B, self.C = -dy, dx, dx * p1.y - dy * p1.x
+		self.xmin, self.xmax = min(p1.x, p2.x), max(p1.x, p2.x)
+		self.ymin, self.ymax = min(p1.y, p2.y), max(p1.y, p2.y)
+
+	def __repr__(self):
+		return "%.3fx + %.3fy = %.3fc" % (self.A, self.B, self.C)
+
+	@staticmethod
+	def Intersection(eq1, eq2):
+		# Solve [A] * [X] = [C] for [X]
+		A = np.array([[eq1.A, eq1.B], [eq2.A, eq2.B]])
+		B = np.array([[eq1.C], [eq2.C]])
+		solution = np.linalg.inv(A).dot(B)
+		x, y = solution[0][0], solution[1][0]
+		if all([
+			eq1.xmin <= x, x <= eq1.xmax, eq2.xmin <= x, x <= eq2.xmax,
+			eq1.ymin <= y, y <= eq1.ymax, eq2.ymin <= y, y <= eq2.ymax]):
+			return Point(x, y)
+		else:
+			return None
+
+class Polygon(object):
+	def __init__(self, points_str):
+		#points_list = eval(points_str)
+		points_list = points_str
+		# Add 0.5 to each point to center it in the box of the pixel
+		# Switch x and y to match 90 degree rotation in configuration web page
+		self.points = [Point(i.x + 0.5, i.y + 0.5) for i in points_list]
+
+	def GenerateMask(self, width, height):
+		# Make the data type 8 bit unsigned integer to match the output of the camera stream
+		mask = np.zeros((height, width, 3), dtype="uint8")
+
+		# Encode the points into linear equations
+		equations = [Equation(p1, p2)
+			for p1, p2 in zip(self.points, self.points[1:] + self.points[:1])]
+
+		# For each row (y), find the (x) values where there is an intersection with a polygon line
+		#     (this also means checking the domain of the polygon line)
+		# For each interval, determine if inside or outside, and fill mask accordingly
+		for y in range(height):
+			# Define the equation for the horizontal line
+			intersections = []
+			hor_eq = Equation(Point(0, y), Point(width, y))
+			for eq in equations:
+				intersection = Equation.Intersection(hor_eq, eq)
+				if intersection is not None:
+					intersections.append(intersection)
+			intersections = sorted(intersections, key=lambda intersection: intersection.x)
+
+			fill = 0
+			index = 0
+			for intersection in [round(i.x) for i in intersections] + [width]:
+				while index < intersection:
+					mask[y, index] = [fill, fill, fill]
+					index += 1
+				fill = int(not fill)
+		return mask
